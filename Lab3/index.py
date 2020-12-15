eb7d55379ef03de6842636e50f36e3ab71a5fb80
SelyankinFyodor <fedkahuligan@gmail.com>
2020-12-15 09:20:33 +0300

    add part3

--- Lab3/index.py
+++ Lab3/index.py
@@ -1,15 +1,19 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.spatial.distance import cdist
-from Lab3.svm import svm
+import math
+from Lab3.svm import svm, decision_func
 
 
 def lin_kern(x, y):
 	return x.dot(y)
 
 
-def rbf_kern(x, y):
-	return np.exp(-cdist(x, y)/2)
+def poly_kern(x, y):
+	return (1+x.dot(y)).dot((1+x.dot(y))).dot((1+x.dot(y)))
+
+
+def gauss_kernel(x1, x2, sigma=1):
+	return math.exp(-np.linalg.norm(x1-x2) / (2 * sigma ** 2))
 
 
 def main1():
@@ -24,8 +28,12 @@ def main1():
 	A2 = np.random.multivariate_normal(m2, cov, size2).transpose()
 	A12 = np.concatenate((A1, A2), axis=1)
 	classes = np.array([-1.0 if i >= size1 else 1.0 for i in range(size1 + size2)])
-	alpha, sv_ids, w, b, sv_x, sv_y = svm(train=A12.transpose(), classes=classes, kern=lin_kern, C=100000,
-		threshold=1e-3)
+	alpha, sv_ids, w, b, sv_x, sv_y = svm(
+		train=A12.transpose(),
+		classes=classes,
+		kern=lin_kern,
+		C=100000,
+		threshold=1e-3, part=1)
 
 	plt.figure(2)
 	x = np.linspace(-4, 5, 10)
@@ -40,6 +48,9 @@ def main1():
 		label="the elements of a sample")
 	plt.scatter(sv_x[:, 0], sv_x[:, 1], c=sv_y, marker='x', cmap='coolwarm', label="support vectors")
 	plt.legend()
+	plt.xlabel('x')
+	plt.ylabel('y')
+	plt.title('well separated data')
 	plt.grid(True)
 	plt.show()
 
@@ -47,5 +58,107 @@ def main1():
 	print(2 / np.linalg.norm(w))
 
 
+def main2():
+	m1 = (2, 2)
+	m2 = (-1, -1)
+	cov = [
+		[1, 0.7],
+		[0.7, 1]
+	]
+	size1, size2 = 50, 50
+	A1 = np.random.multivariate_normal(m1, cov, size1).transpose()
+	A2 = np.random.multivariate_normal(m2, cov, size2).transpose()
+	A12 = np.concatenate((A1, A2), axis=1)
+	classes = np.array([-1.0 if i >= size1 else 1.0 for i in range(size1 + size2)])
+	join = []
+	x = np.linspace(-4, 5, 10)
+	for C, c in zip([1, 10, 100, 1000], ['c', 'g', 'm', 'y']):
+		alpha, sv_ids, w, b, sv_x, sv_y = svm(
+			train=A12.transpose(),
+			classes=classes,
+			kern=lin_kern,
+			C=C,
+			threshold=1e-3,
+			part=1)
+		print("w:", w)
+		print(2 / np.linalg.norm(w))
+		y = (-b - w[0] * x) / w[1]
+		y_border_1 = (-b - 1 - w[0] * x) / w[1]
+		y_border_2 = (-b + 1 - w[0] * x) / w[1]
+		join.append({'y': y, 'b1': y_border_1, 'b2': y_border_2, 'sv_x': sv_x, 'sv_y': sv_y})
+		plt.figure(2)
+		plt.scatter(A12.transpose()[:, 0], A12.transpose()[:, 1], c=classes, marker='.', cmap='coolwarm',
+			label="the elements of a sample")
+		plt.plot(x, y, 'k-')
+		plt.plot(x, y_border_1, c+'-.')
+		plt.plot(x, y_border_2, c+'-.')
+		plt.scatter(sv_x[:, 0], sv_x[:, 1], c=sv_y, marker='x', cmap='coolwarm', label="support vectors")
+		plt.xlabel('x')
+		plt.ylabel('y')
+		plt.title(f'bad separated data, c={C}')
+		plt.grid(True)
+		plt.show()
+
+	plt.figure(2)
+	plt.scatter(A12.transpose()[:, 0], A12.transpose()[:, 1], c=classes, marker='.', cmap='coolwarm',
+		label="the elements of a sample")
+	for data, c in zip(join, ['c', 'g', 'm', 'y']):
+		plt.plot(x, data['y'], 'k-')
+		plt.plot(x, data['b1'], c + '-.')
+		plt.plot(x, data['b2'], c + '-.')
+		plt.scatter(data['sv_x'][:, 0], data['sv_x'][:, 1], c=data['sv_y'], marker='x', cmap='coolwarm', label="support vectors")
+	plt.xlabel('x')
+	plt.ylabel('y')
+	plt.title(f'bad separated data')
+	plt.grid(True)
+	plt.show()
+
+
+def main3():
+	m1 = (0, 0)
+	cov = [
+		[1, 0],
+		[0, 1]
+	]
+	size = 100
+	A = np.random.multivariate_normal(m1, cov, size)
+	classes = np.array([-1.0 if (A[i][0] > 0) ^ (A[i][1] > 0) else 1.0 for i in range(size)])
+	for C in [1, 10, 100, 1000]:
+		alpha, sv_x, sv_classes = svm(train=A, classes=classes, kern=gauss_kernel, C=C,
+			threshold=1e-3)
+		h = .1
+		x_min, x_max = A[:, 0].min() - .5, A[:, 0].max() + .5
+		y_min, y_max = A[:, 1].min() - .5, A[:, 1].max() + .5
+		xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+		x_grid = xx.ravel()
+		y_grid = yy.ravel()
+		xy = np.c_[x_grid, y_grid]
+
+		z_grid = np.zeros(x_grid.shape[0])
+		for i in range(x_grid.shape[0]):
+			z_grid[i] = decision_func(x1=A, x2=xy[i], a=alpha, n_samples=size, y=classes, kern=gauss_kernel)
+
+		plt.figure(1)
+		Z = z_grid.reshape(xx.shape)
+		plt.contourf(xx, yy, Z, alpha=.8)
+		plt.scatter(A[:, 0], A[:, 1], c=classes, marker='.', cmap='coolwarm')
+		plt.scatter(sv_x[:, 0], sv_x[:, 1], c=sv_classes, marker='x', cmap='coolwarm')
+		plt.title(f"C={C}")
+		plt.grid(True)
+		plt.show()
+
+
+def main4():
+	return
+
+
+def main5():
+	return
+
+
 if __name__ == "__main__":
-	main1()
+	# main1()
+	# main2()
+	# main3()
+	main4()
--- Lab3/svm.py
+++ Lab3/svm.py
@@ -1,16 +1,15 @@
 import numpy as np
 import cvxopt
-from qpsolvers import solve_qp
 
 
-def svm(train, classes, kern, C, threshold):
+def svm(train, classes, kern, C, threshold, part=3):
 	n_samples, n_features = train.shape
 	K = np.zeros((n_samples, n_samples))
 	for i in range(n_samples):
 		for j in range(n_samples):
 			K[i, j] = kern(train[i], train[j])
 	P = cvxopt.matrix(np.outer(classes, classes) * K)
-	q = cvxopt.matrix(np.ones(n_samples) * -1)
+	q = cvxopt.matrix(-np.ones(n_samples))
 	A = cvxopt.matrix(classes, (1, n_samples))
 	b = cvxopt.matrix(.0)
 	G = cvxopt.matrix(
@@ -19,19 +18,36 @@ def svm(train, classes, kern, C, threshold):
 	h = cvxopt.matrix(
 		np.concatenate((np.zeros(n_samples), np.full(n_samples, C)), axis=0)
 	)
-	solution = cvxopt.solvers.qp(P, q, G, h, A, b)
-	a = np.ravel(solution['x'])
+	solver = cvxopt.solvers
+	solver.options['show_progress'] = False
+	a = np.ravel(solver.qp(P, q, G, h, A, b)['x'])
 	sv = a > threshold
 	ind = np.arange(len(a))[sv]
-	a = a[sv]
-	sv_train = train[sv]
-	sv_classes = classes[sv]
-	b = 0
-	for n in range(len(a)):
-		b += sv_classes[n]
-		b -= np.sum(a * sv_classes * K[ind[n], sv])
-	b /= len(a)
-	w = np.zeros(n_features)
-	for n in range(len(a)):
-		w += a[n] * sv_classes[n] * sv_train[n]
-	return a, ind, w, b, sv_train, sv_classes
+	if part == 1 or part == 2:
+		a = a[sv]
+		sv_train = train[sv]
+		sv_classes = classes[sv]
+		b = 0
+		for n in range(len(a)):
+			b += sv_classes[n]
+			b -= np.sum(a * sv_classes * K[ind[n], sv])
+		b /= len(a)
+		w = np.zeros(n_features)
+		for n in range(len(a)):
+			w += a[n] * sv_classes[n] * sv_train[n]
+		return a, ind, w, b, sv_train, sv_classes
+	if part == 3:
+		sv_x = []
+		sv_classes = []
+		for index in ind:
+			sv_x.append(train[index])
+			sv_classes.append(classes[index])
+		sv_x = np.array(sv_x)
+		return a, sv_x, sv_classes
+
+
+def decision_func(x1, x2, n_samples, a, y, kern):
+	res = 0
+	for i in range(n_samples):
+		res += a[i] * y[i] * kern(x1[i], x2)
+	return res
